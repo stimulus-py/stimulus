656b183a45e54c46501134844318dc1df59dde03
Michael Herman <mrherman@stimulus-py.com>
2022-09-21 14:35:12 -0400

    add debugging to docker run script

--- run.sh
+++ run.sh
@@ -4,4 +4,13 @@ if [ -f /stimulus/user/config/requirements.txt ]; then
     pip install $PIP_FLAGS -r /stimulus/user/config/requirements.txt
 fi
 
-python ./src/start_stimulus.py
+if [ "$DEBUG" = "ON" ]; then
+    echo "Running with debug on"
+    python -m debugpy --listen 0.0.0.0:3000 ./src/start_stimulus.py
+elif [ "$DEBUG" "HOLD" ]; then
+    echo "Running with debug hold"
+    python -m debugpy --listen 0.0.0.0:3000 --wait-for-client ./src/start_stimulus.py
+else
+    python ./src/start_stimulus.py    
+fi
+
